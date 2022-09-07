eff96f3c15141566d65f8f9d73eecc537ec19952
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-07 10:49:04 +0200

    fetch for card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
 
 const Home = () => {
-  return <div>test</div>;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticle();
+  }, []);
+
+  const fetchArticle = async () => {
+    const request = await fetch(`http://localhost:5000/products`);
+    const response = await request.json();
+    setArticles(response);
+  };
+  console.log(articles);
+  return (
+    <>
+      {articles.map((article) => {
+        return <Card article={article} />;
+      })}
+    </>
+  );
 };
 
 export default Home;
