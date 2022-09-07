0e45843be80aa85eee7accdb9fa2134054886a13
JHanslik <julien.hanslik@gmail.com>
2022-09-07 10:41:01 +0200

    component container added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Container from "./components/Container";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path={"/"} element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Container>
+                <Header />
+                <Routes>
+                    <Route path={"/"} element={<Home />} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    );
 };
 
 export default App;
