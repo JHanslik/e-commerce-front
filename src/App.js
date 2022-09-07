8e5ed384169d6a10d7d4f524f42b80842af1d510
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-07 10:15:29 +0200

    router ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-    return <div className="text-red-700">hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
