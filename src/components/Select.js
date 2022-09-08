220842c1b257433b24960900ff8c181d01b55870
JHanslik <julien.hanslik@gmail.com>
2022-09-08 11:10:20 +0200

    changed select color and background color

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,22 +1,37 @@
 import React from "react";
 
-const Select = ({label, handleChange, options, value}) => {
+const Select = ({ label, handleChange, options, value }) => {
     return (
-       <>
-        <section className="flex justify-center py-0.5 ">
-            <div className="py-4 bg-gray-50 px-5 rounded ">
-                <label className="block mb-2.5 text-m "  htmlFor="city-select">{label}</label>
+        <>
+            <section className="flex justify-center py-0.5 text-center">
+                <div className="py-4 bg-transparent px-5 rounded ">
+                    <label
+                        className="block mb-2.5 text-m "
+                        htmlFor="city-select"
+                    >
+                        {label}
+                    </label>
 
-                <select onChange={handleChange} value={value} className="min-w-[1080px] border border-slate-300 rounded w-12/12 p-1 focus:outline-none focus:border-gray-400 focus:ring-1 focus:ring-gray-400" name="cities" id="city-select">
-                <option value=''>Select a category</option>
+                    <select
+                        onChange={handleChange}
+                        value={value}
+                        className=" border bg-gray-800 text-gray-200 border-slate-300 rounded-lg w-12/12 p-1 focus:outline-none focus:border-gray-400 focus:ring-1 focus:ring-gray-400"
+                        name="cities"
+                        id="city-select"
+                    >
+                        <option value="">Select a category</option>
                         {options.map((option) => {
-                            return <option key={option.value} value={option.value}>{option.text}</option>
+                            return (
+                                <option key={option.value} value={option.value}>
+                                    {option.text}
+                                </option>
+                            );
                         })}
-                </select>
-            </div>
-        </section>
-    </>
-    )
-}
+                    </select>
+                </div>
+            </section>
+        </>
+    );
+};
 
-export default Select
+export default Select;
